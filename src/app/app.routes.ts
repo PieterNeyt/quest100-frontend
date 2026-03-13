129f7a90807107e0de08c4a25fb9758837750d60
Cian Van Acker <cian.vanacker@student.kdg.be>
2026-03-13 20:33:13 +0100

    kleine fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,7 +22,6 @@ export const routes: Routes = [
   {path: 'event', component: EventComponent, canActivate: [MsalGuard]},
   {path: 'attendance/:classId', component: AttendanceComponent, canActivate: [MsalGuard]},
   {path: 'qrcode', component: Qrcode, canActivate: [MsalGuard, roleGuard], data: {role: 'lector'}},
-  {path: 'attendance/:classId', component: AttendanceComponent, canActivate: [MsalGuard]},
   {path: 'userlist', component: Userlist, canActivate: [MsalGuard]},
   {path: 'avatar', component: Avatar, canActivate: [MsalGuard]},
   { path: 'gotcha', component: GotchaPageComponent, canActivate: [MsalGuard] },
