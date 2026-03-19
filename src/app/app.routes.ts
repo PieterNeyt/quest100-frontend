20f34bebc7224dd36a3511e5a452798ca9def2b3
Cian Van Acker <cian.vanacker@student.kdg.be>
2026-03-19 17:13:17 +0100

    rollen restrictie + admin rol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,14 +18,21 @@ import {Dashboard} from './dashboard/dashboard';
 import {reportEvent} from './dashboard/report-event/report-event';
 import {reportChat} from './dashboard/report-chat/report-chat';
 import {ReportAward} from './dashboard/report-award/report-award';
+import {roleGuard} from './guards/role-guard';
+import {Role} from './model/role';
 
 export const routes: Routes = [
   {path: '', component: Home},
   {path: 'profile', component: Profile, canActivate: [MsalGuard]},
   {path: 'event/:eventId', component: EventDetailComponent, canActivate: [MsalGuard]},
   {path: 'event', component: EventComponent, canActivate: [MsalGuard]},
-  {path: 'attendance/:classId', component: AttendanceComponent, canActivate: [MsalGuard]},
-  {path: 'qrcode', component: Qrcode, canActivate: [MsalGuard]},
+  {
+    path: 'attendance/:classId',
+    component: AttendanceComponent,
+    canActivate: [MsalGuard, roleGuard],
+    data: {role: Role.STUDENT}
+  },
+  {path: 'qrcode', component: Qrcode, canActivate: [MsalGuard, roleGuard], data: {role: Role.STUDENT}},
   {path: 'userlist', component: Userlist, canActivate: [MsalGuard]},
   {path: 'avatar', component: Avatar, canActivate: [MsalGuard]},
   {path: 'kudo-overview', component: KudoOverview, canActivate: [MsalGuard]},
@@ -33,10 +40,22 @@ export const routes: Routes = [
   {path: 'gotcha/end', component: GotchaEndPageComponent, canActivate: [MsalGuard]},
   {path: 'gotcha/end/:gameId', component: GotchaEndPageComponent, canActivate: [MsalGuard]},
   {path: 'gotcha/history', component: GotchaHistoryPageComponent, canActivate: [MsalGuard]},
-  {path: 'gotcha/settings', component: GotchaSettingsComponent, canActivate: [MsalGuard]},
+  {
+    path: 'gotcha/settings',
+    component: GotchaSettingsComponent,
+    canActivate: [MsalGuard, roleGuard],
+    data: {role: Role.LECTOR}
+  },
   {path: 'about', component: AboutComponent, canActivate: [MsalGuard]},
-  {path: 'reports/dashboard', component: Dashboard, canActivate: [MsalGuard]},
-  {path: 'report/:reportId/event/:eventId', component: reportEvent, canActivate: [MsalGuard]},
-  {path: 'report/:reportId/message/:messageId/chat/:chatId', component: reportChat, canActivate: [MsalGuard]},
-  {path: 'report/:reportId/award/:targetId', component: ReportAward, canActivate: [MsalGuard]},
+  {
+    path: 'reports',
+    canActivateChild: [MsalGuard, roleGuard],
+    data: {role: Role.ADMIN},
+    children: [
+      {path: 'dashboard', component: Dashboard},
+      {path: ':reportId/event/:eventId', component: reportEvent},
+      {path: ':reportId/message/:messageId/chat/:chatId', component: reportChat},
+      {path: ':reportId/award/:targetId', component: ReportAward}
+    ]
+  },
 ];
